29dc971005e33b584c1259c82f7e3f934c721f5a
Shazil Bukhari <shazilbukhari8@gmail.com>
2026-04-25 12:04:53 +0530

    fix css import

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import "./chat.css"
+import "./Chat.css"
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 
 
